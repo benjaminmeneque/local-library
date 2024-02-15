db17c7ed4dcb5cf5272fbd3adffc5bd9709bf7fa
benjamin <benjamin.cornejo.meneque@gmail.com>
2024-02-15 13:18:09 +0800

    added class based views and index page

---
+++ catalog/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+from django.views import generic
+from .models import Book, BookInstance, Author, Genre
+
+
+# Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_genres = Genre.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact="a").count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    context = {
+        "num_books": num_books,
+        "num_instances": num_instances,
+        "num_instances_available": num_instances_available,
+        "num_authors": num_authors,
+        "num_genres": num_genres,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, "catalog/index.html", context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = "book_list"
+    template_name = "catalog/book_list.html"
+    paginate_by = 2
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = "catalog/book_detail.html"
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    context_object_name = "author_list"
+    template_name = "catalog/author_list.html"
+    paginate_by = 2
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    template_name = "catalog/author_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        author = self.get_object()
+        context["book_instance_count"] = BookInstance.objects.filter(
+            book__author=author
+        ).count()
+        return context
